bc7b829981872cc3f3dd890daeab54209d92c5b7
Pedro Aldeia <pedroaldeia@tecnico.ulisboa.pt>
2024-11-16 11:50:34 +0000

    teste com o iostream do c++

---
+++ projeto1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::string name;
+    std::cout << "olá!" << std::endl;
+    std::cin >> name;
+    std::cout << "olá " << name << std::endl;
+    return 0;
+}
--- relatorio.md
+++ relatorio.md
@@ -1,2 +1,3 @@
 ** Aqui vai estar o relatório (depois temos de passar para word e pdf) **
 
+ainda vai ser publicado um template do relatório portanto podemos esperar até lá
