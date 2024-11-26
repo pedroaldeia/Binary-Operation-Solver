9790b22678e079af1127fc4eec9004f8a0199979
Pedro Aldeia <pedroaldeia@tecnico.ulisboa.pt>
2024-11-26 15:55:19 +0000

    added main loop with comments, not tested

--- projeto1.cpp
+++ projeto1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 char FILENAME[13] = "filename.txt";
@@ -10,6 +11,7 @@ int main(){
     FILE *file;
     char line[BUFFER];
     char* ptr;
+    bool output = false;
 
     file = fopen(FILENAME, "r");
     if (file == NULL) {
@@ -62,7 +64,44 @@ int main(){
 
     /* Loop grande que preenche o resto dos valores */
 
+    /* itera a diagonal da matriz do puzzle */
+    for(int diagonal = 2; diagonal < puzzle_size; diagonal ++){
 
+        /* itera a linha na diagonal */
+        for(int line = 0; line + diagonal < puzzle_size; line ++){
+
+            /* vai buscar a celula para ser preenchida */
+            int column = diagonal + line;
+            vector<int> cell = values_table[line][column];
+
+            /* itera sobre as linhas e colunas para calcular o resultado da celula */
+            for(int table_searcher = 1; table_searcher < diagonal; table_searcher++){
+
+                /* vão ser cada célula das linhas e colunas da célula que queremos calcular */
+                vector<int> celula_coluna = values_table[column-table_searcher][column];
+                vector<int> celula_linha = values_table[line][column-table_searcher];
+
+                /* itera sobre os valores contidos na célula da mesma linha */
+                for(int line_values_iterator = 0; line_values_iterator < celula_linha.size() && cell.size() <= matrix_size; line_values_iterator++){
+
+                    /* itera sobre os valores contidos na célula da mesma coluna */
+                    for(int col_values_iterator = 0; col_values_iterator < celula_linha.size() && cell.size() <= matrix_size; col_values_iterator++){
+                        /* calcula o novo valor da célula */
+                        int value = matrix[celula_linha[line_values_iterator]][celula_coluna[col_values_iterator]];
+                        
+                        /* verifica se o valor já está contido no vetor de soluções da célula */
+                        if(any_of(cell.begin(), cell.end(), [value](int x){ return x == value;})){
+                            cell.push_back(value);
+                        }
+                        /* verifica se o resultado final é sasfeito*/
+                        if(diagonal == puzzle_size-1 && value == intended_result)
+                            printf("YAY, chegaste ao resultado");
+                            output = true;
+                    }
+                }
+            }
+        }
+    }
 
     
 
