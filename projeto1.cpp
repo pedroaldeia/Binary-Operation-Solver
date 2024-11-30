fcf6d16270eb20586940c1125660147247d2c57d
Pedro Aldeia <pedroaldeia@tecnico.ulisboa.pt>
2024-11-30 13:22:15 +0000

    working for the first test, will test more thoroughly

--- projeto1.cpp
+++ projeto1.cpp
@@ -44,7 +44,6 @@ int main(){
     ptr = line;
     for(int i = 0; i < puzzle_size; i++){
         sscanf(ptr, "%d", &puzzle[i]);
-        cout << puzzle[i];
         while(*ptr != ' ' && *ptr != '\0') ptr++;
         ptr++;
     } 
@@ -72,30 +71,29 @@ int main(){
 
             /* vai buscar a celula para ser preenchida */
             int column = diagonal + line;
-            vector<int> cell = values_table[line][column];
 
             /* itera sobre as linhas e colunas para calcular o resultado da celula */
-            for(int table_searcher = 1; table_searcher < diagonal; table_searcher++){
+            for(int table_searcher = 1; table_searcher <= diagonal; table_searcher++){
 
                 /* vão ser cada célula das linhas e colunas da célula que queremos calcular */
-                vector<int> celula_coluna = values_table[column-table_searcher][column];
+                vector<int> celula_coluna = values_table[column-table_searcher+1][column];
                 vector<int> celula_linha = values_table[line][column-table_searcher];
 
                 /* itera sobre os valores contidos na célula da mesma linha */
-                for(int line_values_iterator = 0; line_values_iterator < celula_linha.size() && cell.size() <= matrix_size; line_values_iterator++){
+                for(int line_values_iterator = 0; line_values_iterator < celula_linha.size() && values_table[line][column].size() <= matrix_size && !output; line_values_iterator++){
 
                     /* itera sobre os valores contidos na célula da mesma coluna */
-                    for(int col_values_iterator = 0; col_values_iterator < celula_linha.size() && cell.size() <= matrix_size; col_values_iterator++){
+                    for(int col_values_iterator = 0; col_values_iterator < celula_coluna.size() && values_table[line][column].size() <= matrix_size && !output; col_values_iterator++){
                         /* calcula o novo valor da célula */
                         int value = matrix[celula_linha[line_values_iterator]-1][celula_coluna[col_values_iterator]-1];
                         
                         /* verifica se o valor já está contido no vetor de soluções da célula */
-                        if(!any_of(cell.begin(), cell.end(), [value](int x){ return x == value;})){
-                            cell.push_back(value);
+                        if(!any_of(values_table[line][column].begin(), values_table[line][column].end(), [value](int x){ return x == value;})){
+                            values_table[line][column].push_back(value);
                         }
                         /* verifica se o resultado final é sasfeito*/
                         if(diagonal == puzzle_size-1 && value == intended_result){
-                            printf("YAY, chegaste ao resultado");
+                            printf("YAY, chegaste ao resultado\n");
                             output = true;
                         }
                     }
@@ -107,6 +105,8 @@ int main(){
     // guardar a posição do parentesis e os valores obtidos as sub operações, ex: (2 2)3 -> guardar valor de 2+2, 3 e a posição do parentesis (2)
 
     /* Abaixo são só testes para verificar o output */
+
+    /*
 
     cout << "The entered matrix size: " << matrix_size << endl;
     cout << "The entered puzzle size: " << puzzle_size << endl;
@@ -124,7 +124,11 @@ int main(){
     
 
     cout << endl << "The entered puzzle result: " << intended_result << endl;
-    
+
+    */
+
+    if(output) cout << "true\n";
+    else cout << "false\n";   
 
     return 0;
 }
